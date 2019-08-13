991d78e5b8df171cd5e5727fa3e618529f000653
Red <vhanchar@ucsc.edu>
2019-08-13 14:51:03 -0700

    added reducer to the form

--- reducer-todo/src/components/ToDoForm.js
+++ reducer-todo/src/components/ToDoForm.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState, useReducer} from 'react';
+import {Reducer, initialState} from '../reducers/Reducer';
 
 const ToDoForm = () => {
+
+    const [toDoTask, setToDoTask] = useState();
+    const [state, dispatch] = useReducer(Reducer, initialState);
+    console.log(state);
+
     return(
         <form>
         <input type='text' placeholder='to do...'/> 
