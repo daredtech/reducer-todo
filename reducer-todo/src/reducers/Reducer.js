dc6daeca14015edb4b2b613f035eccab2e167ce6
Red <vhanchar@ucsc.edu>
2019-08-13 14:10:57 -0700

    created an initial state and reducer

---
+++ reducer-todo/src/reducers/Reducer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// initial State
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+
+// basic Reducer
+export const Reducer = (state, action) => {
+    return state;
+}
